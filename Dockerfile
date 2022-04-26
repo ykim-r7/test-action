1fb17af8eedf803d3e5811be9ca88930c7e99c88
Yvonne Kim <yvonne_kim@rapid7.com>
2022-04-26 15:51:42 -0500

    test

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@
 FROM "public.ecr.aws/divvycloud/mimics:latest"
 COPY entrypoint.sh /entrypoint.sh
 COPY example.sarif example.sarif
+COPY example.yml example.yml
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,5 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-echo "$*"
-echo "$0"
-echo "$1"
-echo "$2"
-echo "$3"
 
-stdout=$(mimics $*)
-# echo $stdout
-echo "::set-output name=stdout::$stdout"
-echo "::set-output name=stdout::$stdout"
-echo "::set-output name=stdout::$stdout"
-echo "::set-output name=stdout::$stdout"
-echo "::set-output name=stdout::$stdout"
-echo "::set-output name=stdout::$stdout"
+mimics $*
