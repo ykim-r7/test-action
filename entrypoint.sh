191095afe8e9a59cbe323c5896bc5522b1f35182
Yvonne Kim <yvonne_kim@rapid7.com>
2022-04-26 12:31:48 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,14 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-echo `$*`
+echo "$*"
+echo "$0"
+echo "$1"
+echo "$2"
+echo "$3"
 echo "hi"
+
+mimics version
 # stdout=$(mimics $1)
 # echo $stdout
 # echo "::set-output name=stdout::$stdout"
