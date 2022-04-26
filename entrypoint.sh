350ea2969d67e28e2c50e343396ccef3d97d3afa
Yvonne Kim <yvonne_kim@rapid7.com>
2022-04-26 12:14:35 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-stdout=$(mimics scan --api-key=$1 $2)
+stdout=$(mimics version)
 echo $stdout
 echo "::set-output name=stdout::$stdout"
