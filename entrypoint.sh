ce3232a42ca10655ce28746185c19e8998a46595
Yvonne Kim <yvonne_kim@rapid7.com>
2022-05-04 18:21:55 -0500

    workflow reuse

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 mimics version
-echo "$1 $2 $3 $4"
-sh -c "echo $*"
+#echo "$1 $2 $3 $4"
+#sh -c "echo $*"
 echo $1
-sh -c "mimics $1 $2 $3 $4"
+#sh -c "mimics $1 $2 $3 $4"
